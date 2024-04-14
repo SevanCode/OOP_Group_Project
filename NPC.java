e214f53180df87dfcc0f0ff23c2350782beed4cd
SevanCode <161444976+SevanCode@users.noreply.github.com>
2024-04-15 00:03:45 +0400

    Delete NPC.java

--- NPC.java
+++
@@ -1,8 +0,0 @@
-public class NPC {
-    private int colorOfShirt;
-    private int colorOfHair;
-    private int colorOfShoe;
-    private int colorOfPants;
-    private int gender;
-    // 0 for female and 1 for male
-}
